97623ef31a9dfa121be48033e2153fc0b2357344
Alex Young <alex@heuris.io>
2017-12-15 15:39:08 +0000

    handle settings a new members stats

--- services/stats.go
+++ services/stats.go
@@ -79,11 +79,17 @@ func (s DiskStatsService) Increment(member string) error {
 	return s.db.Update(func(tx *bolt.Tx) error {
 		b := tx.Bucket([]byte("members"))
 		v := b.Get([]byte(member))
-		n, err := strconv.ParseInt(string(v), 0, 0)
-		if err != nil {
-			return fmt.Errorf("could not parse int: %v", err)
+		var n int64
+		if v == nil {
+			n = 1
+		} else {
+			val, err := strconv.ParseInt(string(v), 0, 0)
+			if err != nil {
+				return fmt.Errorf("could not parse int: %v", err)
+			}
+			val++
+			n = val
 		}
-		n++
 		v = []byte(strconv.FormatInt(n, 10))
 		return b.Put([]byte(member), v)
 	})
