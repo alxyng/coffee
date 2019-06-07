ea4cca0261cf92ed5a23cd78d3b540264ba400fc
Matt Holland <matt.holland@myunidays.com>
2019-06-07 15:14:52 +0100

    Actually make it compile.

--- handlers/coffee/handler.go
+++ handlers/coffee/handler.go
@@ -129,7 +129,7 @@ func (h CoffeeHandler) getMemberStatsWithNames(stats map[string]int) ([]string, 
 	}
 
 	var results []memberStats
-	for _ = range stats {
+	for range stats {
 		s := <-ch
 
 		if s.Error != nil {
@@ -143,10 +143,12 @@ func (h CoffeeHandler) getMemberStatsWithNames(stats map[string]int) ([]string, 
 
 	var output []string
 	for _, r := range results {
-		output = append(output, fmt.Sprintf("%v: %v", r.Name, r.CoffeesMade))
+		pattern := "%v: %v"
 		if r.CoffeesMade == 69 {
-			output += " :archer:"
+			pattern += " :archer:"
 		}
+
+		output = append(output, fmt.Sprintf("%v: %v", r.Name, r.CoffeesMade))
 	}
 
 	return output, nil
